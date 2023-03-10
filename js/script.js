0e774194d179c79daf048a7930900d6380b6e515
Samuel-nilsson-nti-johanneberg <samuel.nilsson2@elev.ga.ntig.se>
2023-03-10 12:57:19 +0100

    alla pushar jag glömt

--- js/script.js
+++ js/script.js
@@ -8,3 +8,49 @@ function toggleMenu() {
 
 
 burger.addEventListener('click', toggleMenu)
+
+
+
+
+// Galleriswitch funktionen
+// Väljer alla knappar som switchar mellan gallerierna och alla gallerier
+const galleryButtons = document.querySelectorAll('.gallery-button');
+const galleries = document.querySelectorAll('.gallery');
+
+// Går igenom varje knapp och lägger till en click-eventlyssnare
+galleryButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Väljer det galleri som matchar knappens dataset-attribut
+    const gallery = document.querySelector(`#${button.dataset.gallery}`);
+    // Tar bort "active"-klassen från alla gallerier
+    galleries.forEach(gallery => gallery.classList.remove('active'));
+    // Lägger till "active"-klassen på det valda galleriet
+    gallery.classList.add('active');
+  });
+});
+
+
+
+
+
+
+var slides = ['slide1', 'slide2', 'slide3', 'slide4', 'slide5'];
+var currentSlide = 0;
+
+function nextSlide() {
+  var slide = document.getElementsByClassName('slide')[currentSlide];
+  slide.classList.remove('active');
+  currentSlide = (currentSlide + 1) % slides.length;
+  slide = document.getElementsByClassName('slide')[currentSlide];
+  slide.classList.add('active');
+}
+
+function prevSlide() {
+  var slide = document.getElementsByClassName('slide')[currentSlide];
+  slide.classList.remove('active');
+  currentSlide = (currentSlide + slides.length - 1) % slides.length;
+  slide = document.getElementsByClassName('slide')[currentSlide];
+  slide.classList.add('active');
+}
+
+var slideInterval = setInterval(nextSlide, 4000);
