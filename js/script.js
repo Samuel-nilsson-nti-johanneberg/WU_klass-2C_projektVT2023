eb882f524384ae5394c03ef010d873627a808680
Samuel-nilsson-nti-johanneberg <samuel.nilsson2@elev.ga.ntig.se>
2023-03-28 14:31:00 +0200

    lektionsarbetet 28 mars

--- js/script.js
+++ js/script.js
@@ -11,27 +11,6 @@ burger.addEventListener('click', toggleMenu)
 
 
 
-
-// Galleriswitch funktionen
-// Väljer alla knappar som switchar mellan gallerierna och alla gallerier
-const galleryButtons = document.querySelectorAll('.gallery-button');
-const galleries = document.querySelectorAll('.gallery');
-
-// Går igenom varje knapp och lägger till en click-eventlyssnare
-galleryButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    // Väljer det galleri som matchar knappens dataset-attribut
-    const gallery = document.querySelector(`#${button.dataset.gallery}`);
-    // Tar bort "active"-klassen från alla gallerier
-    galleries.forEach(gallery => gallery.classList.remove('active'));
-    // Lägger till "active"-klassen på det valda galleriet
-    gallery.classList.add('active');
-  });
-});
-
-
-
-
 // Här kommer koden till bildspelet på contact-sidan
 
 var slides = ['slide1', 'slide2', 'slide3', 'slide4', 'slide5'];
@@ -54,3 +33,24 @@ function prevSlide() {
 }
 
 var slideInterval = setInterval(nextSlide, 4000);
+
+
+// Jag valde att spara koden nedanför men att kommentera ut den. Koden fungerar inte och jag hinner inte fixa den.
+// Men jag känner att det är dumt att ta bort koden då jag kanske kan fixa den på fördjupningsdelen
+
+// // Galleriswitch funktionen
+// // Väljer alla knappar som switchar mellan gallerierna och alla gallerier
+// const galleryButtons = document.querySelectorAll('.gallery-button');
+// const galleries = document.querySelectorAll('.gallery');
+
+// // Går igenom varje knapp och lägger till en click-eventlyssnare
+// galleryButtons.forEach(button => {
+//   button.addEventListener('click', () => {
+//     // Väljer det galleri som matchar knappens dataset-attribut
+//     const gallery = document.querySelector(`#${button.dataset.gallery}`);
+//     // Tar bort "active"-klassen från alla gallerier
+//     galleries.forEach(gallery => gallery.classList.remove('active'));
+//     // Lägger till "active"-klassen på det valda galleriet
+//     gallery.classList.add('active');
+//   });
+// });
